1a9346a399de5bf83cd8e218b425722eeefff5f0
Hunt Collings <hunt@collings.co.uk>
2018-12-17 18:38:26 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
+const key = parseInt(e.detail || e.which);
+
+if (key === code[index]) {
+  index ++;
+  
+if index === alphabet.length {
+  alert("Well Done!");
+  
+  index = 0;
+
+}  
+}
 
 
-
-alert("Well Done!")
 
 }
