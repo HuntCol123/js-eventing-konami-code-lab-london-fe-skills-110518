f35ea7f3c8c217c58b5ca29c752e668a39e454bd
Hunt Collings <hunt@collings.co.uk>
2018-12-17 19:14:32 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,30 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  let index = 0;  
   // Write your JavaScript code inside the init() function
+  function onKeyDownHandler (e) {
+    console.log(e);
+    const key = parseInt(e.detail || e.which);
+    console.log(key);
+    console.log(index);
+    
+    if (key === code[index]){
+      index++;
+    
+  if (index === code.length) {
+      alert("Hurray!");  
+    
+    index = 0;
+  }
+    } else {index = 0;}
+  
+  
+  
+}
+  document.body.addEventListener('keydown', onKeyDownHandler)
 
 }
+
+init();
+
