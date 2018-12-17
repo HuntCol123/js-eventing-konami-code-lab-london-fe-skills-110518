ae8fe8e83893051d71faa779db633ef932739e1b
Hunt Collings <hunt@collings.co.uk>
2018-12-17 18:41:28 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,9 +1,9 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
-const main = document.getElementByTagName('main')
+let main = document.getElementByTagName('main')
  
-main.addEventListener('click', function(event) {
+main.addEventListener('keydown', function(event) {
   alert('I was clicked!')
 })
 
