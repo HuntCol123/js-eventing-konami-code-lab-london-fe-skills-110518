d0ae6aea2a74bd5feeb0e35bd3185cc230b604d1
Hunt Collings <hunt@collings.co.uk>
2018-12-17 18:33:16 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -5,4 +5,19 @@ function init() {
 
 let index = 0;
 
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
 }
